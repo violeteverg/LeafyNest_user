f34c05ad3929d65c0f43665bf0c825f54aa5aebd
ojan <milyasa2468@gmail.com>
2024-11-06 10:00:45 +0700

    (USR-16): refactor if session data outside payment route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import { getSessionStorageItem } from "./lib/utils";
 // import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
+  const location = useLocation();
+  const isPaymentPage = location.pathname === "/payment";
+
+  if (!isPaymentPage) {
+    getSessionStorageItem("__Ttemp", true);
+  }
+
   return (
     <>
       <Navbar />
