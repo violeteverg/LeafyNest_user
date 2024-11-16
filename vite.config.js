df7a3bd780d6ef96c195b741e33db1619c691450
ojan <milyasa2468@gmail.com>
2024-11-16 13:38:16 +0700

    (fix): fixing vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
 /* eslint-disable no-undef */
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+      output: {
+        manualChunks: undefined,
+      },
     },
   },
 });
