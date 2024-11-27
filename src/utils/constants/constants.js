1c204c00d1442d9e07b8397803284aabf8e1fc9b
ojan <milyasa2468@gmail.com>
2024-11-27 14:38:23 +0700

    (fix): cactus page api

--- src/utils/constants/constants.js
+++ src/utils/constants/constants.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://final-project-be-mauve.vercel.app/api";
-export const baseUrl = "http://localhost:3000/api/";
+export const baseUrl = "https://final-project-be-mauve.vercel.app/api";
+// export const baseUrl = "http://localhost:3000/api/";
